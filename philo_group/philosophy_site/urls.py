0cb2127bdc64b18c9f58635a9040dafba94871f6
Antony100 <antonyangeli10@gmail.com>
2023-07-19 16:12:04 +0100

    Remove duplicate directory

--- philo_group/philosophy_site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from philo_group import views as philoViews
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('philo_group.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
-]
